8b72a0231f36c86b949baf4237eaf55f97e2b7df
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-10-11 14:32:23 +0900

    [update]add top page

--- src/client/index.js
+++ src/client/index.js
@@ -6,13 +6,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import Top from "./pages/Top";
 import "./scss/style.scss";
 
 ReactDOM.render(
     <BrowserRouter>
         <Header />
         <Switch>
-            
+            <Route path="/" component={ Top }></Route>
         </Switch>
         <Footer />
     </BrowserRouter>,
