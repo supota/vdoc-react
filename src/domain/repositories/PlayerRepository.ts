5a174f3ce8744fa63df5754594cc1a8087f88924
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-12-07 22:53:31 +0900

    Add new method in PlayerRepository

--- src/domain/repositories/PlayerRepository.ts
+++ src/domain/repositories/PlayerRepository.ts
@@ -2,6 +2,8 @@ import Player from "vdoc/domain/models/Player";
 
 abstract class PlayerRepository {
   abstract getAllPlayers(): Promise<Player[]>;
+
+  abstract getPlayer(id: number): Promise<Player>;
 }
 
 export default PlayerRepository;
