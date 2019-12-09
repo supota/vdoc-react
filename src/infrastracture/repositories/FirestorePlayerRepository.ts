a60cf3b5077b95a4d0e8e5cfb4dae0df88b489b1
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-12-10 00:01:28 +0900

    Delete pre-commit and it's test

--- src/infrastracture/repositories/FirestorePlayerRepository.ts
+++ src/infrastracture/repositories/FirestorePlayerRepository.ts
@@ -1,43 +1,47 @@
-import { firebase } from 'vdoc/application/firebase';
+import { firebase } from "vdoc/application/firebase";
 
-import { Player } from 'vdoc/domain/models/Player';
-import { PlayerRepository } from 'vdoc/domain/repositories/PlayerRepository';
+import { Player } from "vdoc/domain/models/Player";
+import { PlayerRepository } from "vdoc/domain/repositories/PlayerRepository";
 
 class FirestorePlayerRepository extends PlayerRepository {
-
   async getAllPlayers(): Promise<Player[]> {
-
     // Get all players
     let playersSnapshot;
     try {
-      playersSnapshot = await firebase.firestore().collection('players').get();
+      playersSnapshot = await firebase
+        .firestore()
+        .collection("players")
+        .get();
     } catch {
-      throw new Error('Could not get players.');
+      throw new Error("Could not get players.");
     }
 
-    // Translate to Player[]
-    const players = playersSnapshot.docs.map((playerSnapshot: firebase.firestore.DocumentSnapshot) => {
-      return new Player(playerSnapshot.data());
-    })
+    // Translate to array of Player
+    const players = playersSnapshot.docs.map(
+      (playerSnapshot: firebase.firestore.DocumentSnapshot) => {
+        return new Player(playerSnapshot.data());
+      }
+    );
 
     return players;
   }
 
   async getPlayer(id: string): Promise<Player> {
-
     // Get player from id
     let playerSnapshot;
     try {
-      playerSnapshot = await firebase.firestore().collection('players').doc(id).get();
+      playerSnapshot = await firebase
+        .firestore()
+        .collection("players")
+        .doc(id)
+        .get();
     } catch {
       throw new Error(`Could not get player ${id}.`);
     }
 
     const player = new Player(playerSnapshot.data());
     return player;
-
   }
-
 }
 
 export { FirestorePlayerRepository };
