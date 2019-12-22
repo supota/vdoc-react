b7c2cef88dce374437d23692782a5ee72fe1dab7
issui <issui.real@gmail.com>
2019-12-22 15:33:34 +0900

    notfoundページ改修

--- src/presentation/pages/NotFound/index.tsx
+++ src/presentation/pages/NotFound/index.tsx
@@ -2,16 +2,17 @@ import * as React from "react";
 
 const NotFoundPage = () => {
   return (
-    <div id="message">
-      <h2>404｜Page Not Found</h2>
-      <h1>ページが見つかりませんでした</h1>
-      <p>
-        指定されたファイルは見つかりませんでした。
-        <br />
-        URLに誤りがないか確認してください。
-      </p>
-
-      <a href="https://v-doc.supota.com"> TOPに戻る</a>
+    <div id="not-found">
+      <div id="message">
+        <h2>404｜Page Not Found</h2>
+        <h1>ページが見つかりませんでした</h1>
+        <p>
+          指定されたファイルは見つかりませんでした。
+          <br />
+          URLに誤りがないか確認してください。
+        </p>
+        <a href="https://v-doc.supota.com"> TOPに戻る</a>
+      </div>
     </div>
   );
 };
