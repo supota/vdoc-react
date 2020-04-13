145f4078a5ef76b237cac68d29be1a24bc542635
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2020-04-13 18:28:44 +0900

    Add comment to domain repository

--- src/libs/domain/repositories/PlayerRepository.ts
+++ src/libs/domain/repositories/PlayerRepository.ts
@@ -2,12 +2,24 @@ import { Player } from "vdoc/libs/domain/models/Player";
 import { Sports } from "vdoc/libs/domain/models/Sports";
 
 abstract class PlayerRepository {
+  /**
+   * 全てのプレイヤーを取得する
+   */
   abstract async getAllPlayers(): Promise<Player[]>;
 
+  /**
+   * IDからプレイヤーを1つ取得する
+   */
   abstract async getPlayer(id: string): Promise<Player>;
 
+  /**
+   * スポーツからプレイヤーを全て取得する
+   */
   abstract async getPlayerFromSports(sports: Sports): Promise<Player[]>;
 
+  /**
+   * プレイヤーを作成する
+   */
   abstract async postPlayer(player: Player): Promise<void>;
 }
 
--- src/libs/domain/repositories/SportsRepository.ts
+++ src/libs/domain/repositories/SportsRepository.ts
@@ -1,7 +1,14 @@
 import { Sports } from "vdoc/libs/domain/models/Sports";
 
 abstract class SportsRepository {
+  /**
+   * 全てのスポーツを取得
+   */
   abstract async getAllSports(): Promise<Sports[]>;
+
+  /**
+   * IDからスポーツを1つ取得
+   */
   abstract async getSports(id: string): Promise<Sports>;
 }
 
