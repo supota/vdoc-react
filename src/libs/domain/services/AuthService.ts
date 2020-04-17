3d8d85eb9378fcea3dcf7b90dfa39af8742c2581
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2020-04-17 15:43:20 +0900

    Fix AuthService

--- src/libs/domain/services/AuthService.ts
+++ src/libs/domain/services/AuthService.ts
@@ -1,10 +1,15 @@
 abstract class AuthService {
   /**
    * ログインする
+   *
    * @param email 登録したメールアドレス
    * @param password 登録したパスワード
    */
-  abstract async login(email: string, password: string): Promise<boolean>;
+  abstract async login(email: string, password: string): Promise<void>;
+  /**
+   * ログインしているかどうか
+   */
+  abstract isLoggedIn(): boolean;
 }
 
 export { AuthService };
--- src/libs/infra/firebase/services/FirebaseAuthService.ts
+++ src/libs/infra/firebase/services/FirebaseAuthService.ts
@@ -3,13 +3,15 @@ import { firebase } from 'vdoc/libs/infra/firebase/firebase';
 import { AuthService } from 'vdoc/libs/domain/services/AuthService';
 
 class FirebaseAuthService extends AuthService {
-  async login(email: string, password: string): Promise<boolean> {
+  async login(email: string, password: string): Promise<void> {
     try {
       await firebase.auth().signInWithEmailAndPassword(email, password);
     } catch {
       throw new Error('Could not verify user.');
     }
+  }
 
+  isLoggedIn(): boolean {
     return firebase.auth().currentUser !== null;
   }
 }
