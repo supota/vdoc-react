52564264dd8ae7deae87fccb897fa360285ebf53
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2020-04-22 22:37:27 +0900

    Change Header button by authState

--- src/presentation/organisms/Header.tsx
+++ src/presentation/organisms/Header.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 import { withRouter } from 'react-router';
 
+import { authSelectors } from 'vdoc/modules/auth';
+
 const Header = withRouter(props => {
+
+  const authState = useSelector(authSelectors.selectAuthState);
+
   return (
     <header>
       <p
@@ -12,11 +18,14 @@ const Header = withRouter(props => {
       >
         V Doc.
       </p>
-      <div className="link">
-        <a href="/login">ログイン</a>
-        <span></span>
-        <a href="/form">選手登録</a>
-      </div>
+      { authState.isLoggedIn
+        ? <div></div>
+        : <div className="link">
+            <a href="/login">ログイン</a>
+            <span></span>
+            <a href="/form">選手登録</a>
+          </div>
+      }
     </header>
   );
 });
