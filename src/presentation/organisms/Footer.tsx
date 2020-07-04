46222af24a176305624b3a78174af935aaa7562d
issui <issui.real@gmail.com>
2020-07-04 20:51:10 +0900

    update footer

--- src/presentation/organisms/Footer.tsx
+++ src/presentation/organisms/Footer.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
+import {Link} from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer">
+        <ul className="footer-list">
+          <li>
+            <a href="mailto:info@supota.com">お問い合わせ</a><span>/</span>
+            <Link to="/privacy">プライバシーポリシー</Link><span>/</span>
+            <Link to="/terms">利用規約</Link>
+          </li>
+        </ul>
         <p className="footer_copyright">©︎2020 SUPOTA Inc.</p>
       </div>
     </footer>
